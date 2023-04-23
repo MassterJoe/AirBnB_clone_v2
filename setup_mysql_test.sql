cb51310a407d2549a6cf14a1417e25e5b28e837c
Salawu O Joseph <sojbaba98@gmail.com>
2023-04-23 03:14:29 -0400

    hmmm

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,7 @@
---Prepares a MySQL test server for the project
+-- Creates a database called hbnb_test_db in the current MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- creates the MySQL server user hbnb_test
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`. * TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- Grants Permissions for user hbnb_test
+GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
