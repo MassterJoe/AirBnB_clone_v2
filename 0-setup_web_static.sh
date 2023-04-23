9050cf64b622365f32a657954d043586ebf33225
Salawu O Joseph <sojbaba98@gmail.com>
2023-04-23 16:35:05 -0400

    shjk

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,21 +9,20 @@ apt-get update
 apt-get install -y nginx
 # fi
 
-# create directories
+
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
-# create a fake html file
 touch /data/web_static/releases/test/index.html
 echo "Holberton School" > /data/web_static/releases/test/index.html
 
-# create a symbolic link
+
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# give owenership to user and group
+
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
-# update the nginx configuration to serve /data/web_static/current/ to hbnb_static
+
 
 printf %s "server {
 listen 80 default_server;
@@ -38,7 +37,7 @@ index index.html index.htm;
 }
 
 location /redirect_me {
-return 301 http://www.google.com/;
+return 301 http://cuberule.com/;
 }
 
 error_page 404 /404.html;
